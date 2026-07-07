d6fbe055991c6d0e5b917ab9e0607eccf7f6392f
Alanis Santos <alanisosantos@outlook.com>
2026-07-07 16:53:17 -0300

    Implementando o CRUD de Categoria

--- src/components/categoria/listacategoria/ListaCategorias.tsx
+++ src/components/categoria/listacategoria/ListaCategorias.tsx
@@ -1,16 +1,57 @@
+import CardCategoria from "../cardcategoria/CardCategoria";
+import { useEffect, useState } from "react";
+import type Categoria from "../../../model/Categoria";
+import { buscar } from "../../../service/Service";
+import { SyncLoader } from "react-spinners";
+
 function ListaCategorias() {
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const [categorias, setCategorias] = useState<Categoria[]>([])
+
+    useEffect(() => {
+        buscarCategorias()
+    }, [categorias.length])
+
+    async function buscarCategorias() {
+        try {
+
+            setIsLoading(true)
+            await buscar('/categorias', setCategorias)
+        } catch (error: any) {
+            setIsLoading(false)
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
     return (
         <>
 
+            {isLoading && (
+                <SyncLoader
+                    color="#312e81"
+                    size={32}
+                />
+            )}
 
             <div className="flex justify-center w-full my-4">
                 <div className="container flex flex-col">
 
-                <span className="text-3xl text-center my-8">
+                    {(!isLoading && categorias.length === 0) && (
+                        <span className="text-3xl text-center my-8">
                             Nenhuma Categoria foi encontrada!
                         </span>
+                    )}
 
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {
+                            categorias.map((categoria) => (
+                                <CardCategoria key={categoria.id} categoria={categoria} />
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
         </>
