4e750b6533f2fc144476438d9e967383ab2fb091
Alanis Santos <alanisosantos@outlook.com>
2026-07-07 15:11:05 -0300

    Construindo components

---
+++ src/components/categoria/listacategoria/ListaCategorias.tsx
@@ -0,0 +1,20 @@
+function ListaCategorias() {
+
+    return (
+        <>
+
+
+            <div className="flex justify-center w-full my-4">
+                <div className="container flex flex-col">
+
+                <span className="text-3xl text-center my-8">
+                            Nenhuma Categoria foi encontrada!
+                        </span>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ListaCategorias;
